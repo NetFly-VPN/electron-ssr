f805160ca387426f89ae7e8568e9b6dbe6fc3f53
chenchanhao <chenchaohan@live.com>
2019-04-27 12:03:46 +0800

    增加科學上網加密方式

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -32,8 +32,8 @@ if (storedMethods) {
   methods = JSON.parse(storedMethods)
 } else {
   methods = ['none', 'aes-128-cfb', 'aes-192-cfb', 'aes-256-cfb', 'aes-128-cfb8', 'aes-192-cfb8', 'aes-256-cfb8',
-    'aes-128-ctr', 'aes-192-ctr', 'aes-256-ctr', 'camellia-128-cfb', 'camellia-192-cfb', 'camellia-256-cfb',
-    'bf-cfb', 'rc4', 'rc4-md5', 'rc4-md5-6', 'salsa20', 'chacha20', 'chacha20-ietf'
+    'aes-128-ctr', 'aes-192-ctr', 'aes-256-ctr', 'aes-128-gcm', 'aes-192-gcm', 'aes-256-gcm', 'camellia-128-cfb', 'camellia-192-cfb', 'camellia-256-cfb',
+    'bf-cfb', 'rc4', 'rc4-md5', 'rc4-md5-6', 'salsa20', 'chacha20', 'chacha20-ietf', 'chacha20-ietf-poly1305'
   ]
   ls.setItem(STORE_KEY_SSR_METHODS, JSON.stringify(methods))
 }
